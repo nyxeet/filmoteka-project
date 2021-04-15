59f21ec2e6a82f9cb439f700f5e190a89a276fd0
sBeznosiuk <67642990+sBeznosiuk@users.noreply.github.com>
2021-04-15 19:25:10 +0300

    01-main

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import './js/modal-window-film';
 import './js/header';
 import './js/render-main';
+import './js/my-library';
---
+++ src/js/my-library.js
@@ -0,0 +1,88 @@
+import api from '../api/tv-api';
+import markupCard from '../templates/movie-card.hbs';
+
+const watchedBtnRef = document.querySelector('#js-watched');
+const queueBtnRef = document.querySelector('#js-queue');
+const controlsRef = document.querySelector('.menu-controls');
+const movieList = document.querySelector('.movies');
+
+function renderWatched() {
+  const id = JSON.parse(localStorage.getItem('watched'));
+  console.log(id);
+
+  const filteredId = id.filter(onlyUnique);
+  console.log(filteredId);
+
+  const type = 'movie';
+
+  filteredId.forEach(item => {
+    api
+      .fetchShowDetails(type, item)
+      .then(res => {
+        const data = parseData(res);
+        return data;
+      })
+      .then(data => {
+        const markup = markupCard(data);
+        movieList.insertAdjacentHTML('beforeend', markup);
+      })
+      .then(() => {
+        api.fetchShowDetails('movie', 634528).then(data => console.log(data));
+      })
+      .then(() => {
+        watchedBtnRef.addEventListener('click', console.log('a'));
+      });
+  });
+}
+
+function renderQueue() {
+  const id = JSON.parse(localStorage.getItem('queue'));
+  console.log(id);
+
+  const filteredId = id.filter(onlyUnique);
+  console.log(filteredId);
+
+  const type = 'movie';
+
+  filteredId.forEach(item => {
+    api
+      .fetchShowDetails(type, item)
+      .then(res => {
+        const data = parseData(res);
+        return data;
+      })
+      .then(data => {
+        const markup = markupCard(data);
+        movieList.insertAdjacentHTML('beforeend', markup);
+      })
+      .then(() => {
+        api.fetchShowDetails('movie', 634528).then(data => console.log(data));
+      })
+      .then(() => {
+        watchedBtnRef.addEventListener('click', console.log('a'));
+      });
+  });
+}
+
+function parseData(data) {
+  const title = data.original_title || data.title || data.original_name;
+  const url = 'https://image.tmdb.org/t/p/w500' + data.poster_path;
+  const year = data.release_date
+    ? data.release_date.slice(0, 4)
+    : data.first_air_date.slice(0, 4);
+  const genresStr =
+    data.genres.map(item => item.name).join(', ') || 'No information';
+
+  return {
+    title,
+    url,
+    year,
+    genresStr,
+  };
+}
+
+function onlyUnique(e, i, a) {
+  return a.indexOf(e) === i;
+}
+
+export { renderWatched };
