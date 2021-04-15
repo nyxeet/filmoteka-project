adb93698d38f31dc0ca31ef82f3933aaeea9f86c
Dmytro Kyrychenko <dmytro.kyrychenko@plus4u.net>
2021-04-15 16:54:19 +0300

    update headers

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,19 @@
 import header from '../html/header.html';
-
+import header2 from '../html/header-mylibrary.html';
 const wrapperRef = document.querySelector('.wrapper');
 
-wrapperRef.insertAdjacentHTML('afterbegin', header);
+wrapperRef.innerHTML = header;
+
+const linkLibrary = document.querySelector('.nav-link-library');
+const linkMain = document.querySelector('.nav-link-home');
+
+linkLibrary.addEventListener('click', event => {
+  event.preventDefault();
+  wrapperRef.innerHTML = header2;
+});
+linkMain.addEventListener('click', event => {
+  event.preventDefault();
+  wrapperRef.innerHTML = header;
+});
 
 // console.log(markup);
