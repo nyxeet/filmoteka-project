3aa824eb82fdf67cbbd561b2087dc5f89bb19a93
Dmytro Kyrychenko <dmytro.kyrychenko@plus4u.net>
2021-04-17 16:29:37 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,11 @@ function renderHomeByQuery(query) {
     .finally(() => placeholder.spinner.close());
 }
 function renderHomePageByPageNum(pageNum) {
-  api.fetchPopularByPage(pageNum).then(({ results }) => renderList(results));
+  placeholder.spinner.show();
+  api
+    .fetchPopularByPage(pageNum)
+    .then(({ results }) => renderList(results))
+    .finally(() => placeholder.spinner.close());
   window.scrollTo(0, 0);
 }
 
