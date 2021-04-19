db29d57b96caa53197eb157e162f0b227b0e6397
Krytoryni <suchgoodidea@gmail.com>
2021-04-19 22:30:25 +0300

    added markup and styles

--- src/js/modal-team-members.js
+++ src/js/modal-team-members.js
@@ -2,5 +2,11 @@ import templateOfTeam from '../templates/modal-team-members.hbs';
 
 const ancorRef = document.querySelector('#team-ancor');
 const markup = templateOfTeam();
+// console.log(ancorRef);
+ancorRef.insertAdjacentHTML('beforeend', markup);
 
-// ancorRef.insertAdjacentHTML('beforeend', markup);
+// class ModalTeam {
+//   constructor(ancor) {
+//     this.ancor = ancor;
+//   }
+// }
